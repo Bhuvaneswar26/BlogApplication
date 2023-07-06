980574d41d6172ce4513e50eb479bc4107483a8a
Bhuvaneswar26 <21951a0527@iare.ac.in>
2023-07-07 01:41:12 +0530

    Initial commit

---
+++ Routes/login.js
@@ -0,0 +1,35 @@
+//importing required packages and modules
+const express = require('express');
+const app = express();
+const route = express.Router();
+const session= require('express-session')
+
+
+const { login, loginverify, logout } = require('../Controllers/login');
+const { signup, signupverify } = require('../Controllers/signup');
+
+
+
+
+
+const isAuth = (req, res, next) => {
+  if (req.session && req.session.isAuth) {
+    res.redirect('/dashboard');
+  } else {
+    next();
+  }
+};
+
+
+
+
+route.get('/login',isAuth,login)
+route.post('/login',isAuth,loginverify)
+
+route.get('/signup',isAuth,signup)
+route.post('/signup', isAuth, signupverify)
+
+route.get('/logout',logout)
+
+
+module.exports = route;
